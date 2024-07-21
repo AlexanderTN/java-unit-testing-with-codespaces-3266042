0259c65d8f7362b9b00280ebd9c3e9d26e5de79a
AlexanderTN <nguyenhoangtam@me.com>
2024-07-21 16:41:53 +0700

    Hairsalon unit test demo

--- src/main/java/HairSalon.java
+++ src/main/java/HairSalon.java
@@ -1,19 +1,24 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class HairSalon {
 
-    private String[] openingDays = {"Monday", "Tuesday"};
+    private final String[] openingDays = {"Monday", "Tuesday"};
 
     // This method should return the openingDays array.
     public String[] getOpeningDays() {
-        return new String[]{};
+        return openingDays;
     }
 
     // This method should return true if the weekday passed in is present in the openingDays array.
     // If not it should return false. If the String passed in does not end in the letters "day",
     // it should throw an IllegalArgumentException with a message explaining that only weekdays are valid.
     public boolean isOpen(String weekDay) {
-        return false;
+        if (!weekDay.endsWith("day")) {
+            throw new IllegalArgumentException("You need to input a weekday");
+        }
+        List<String> openingList = Arrays.asList(openingDays);
+        return openingList.contains(weekDay);
     }
 
 
--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,27 +1,37 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class HairSalonTest {
 
     HairSalon hairSalon = new HairSalon();
 
     @Test
     public void testGetOpeningDays() {
-
+        String[] expectedDays = new String[]{"Monday", "Tuesday"};
+        String[] actualDays = hairSalon.getOpeningDays();
+        assertArrayEquals(expectedDays, actualDays);
     }
 
     @Test
     public void testSalonIsOpenOnMonday() {
-
+        boolean actual = hairSalon.isOpen("Monday");
+        assertTrue(actual);
     }
 
     @Test
     public void testSalonIsClosedOnWednesday() {
-
+        boolean actual = hairSalon.isOpen("Wednesday");
+        assertFalse(actual);
     }
 
     @Test
     public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
-
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+            hairSalon.isOpen("bandeau") );
+        String expectedMessage = "You need to input a weekday";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
     }
 
 }
